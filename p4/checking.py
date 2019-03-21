ed25faae2d467e754dd9aef658ae28fdad8ad122
Deven Damiano <damiano.da96@gmail.com>
2019-03-21 12:51:31 -0400

    added basic type checking functionality

--- p4/checking.py
+++ p4/checking.py
@@ -1,67 +1,125 @@
 from exp import *
 
+def is_bool(e):
+	if(isinstance(e, Type)):
+		return e == BoolType
+	if(isinstance(e, Expr)):
+		return is_bool(check(e))
+
+def is_int(e):
+	if(isinstance(e, Type)):
+		return e == IntType
+	if(isinstance(e, Expr)):
+		return is_int(check(e))
+
+def is_same_type(t1, t2):
+	if type(t1) != type(t2):
+		return False
+
+	if(type(t1) == BoolType or type(t1) == IntType):
+		return True
+
+	assert(False)
+
+def expr_same_type(e1, e2):
+	return is_same_type(type_check(e1), type_check(e2))
+
 def check_bool(e):
-	pass
+	return BoolType
 
 def check_not(e):
     pass
 
 def check_and(e):
-    pass
+    if(is_bool(e.lhs) and is_bool(e.rhs))
+    	return BoolType
+
+    assert(False)
 
 def check_or(e):
-    pass
+    if(is_bool(e.lhs) and is_bool(e.rhs))
+    	return BoolType
 
 def check_int(e):
-	pass
+	return IntType
 
 def check_if(e):
 	pass
 
 def check_add(e):
-	pass
+	if(is_int(e.lhs) and is_int(e.rhs)):
+		return IntType
+
+	assert False
 
 def check_sub(e):
-	pass
+	if(is_int(e.lhs) and is_int(e.rhs)):
+		return IntType
+
+	assert False
 
 def check_mult(e):
-	pass
+	if(is_int(e.lhs) and is_int(e.rhs)):
+		return IntType
+
+	assert False
 
 def check_div(e):
-	pass
+	if(is_int(e.lhs) and is_int(e.rhs)):
+		return IntType
+
+	assert False
 
 def check_mod(e):
-	pass
+	if(is_int(e.lhs) and is_int(e.rhs)):
+		return IntType
+
+	assert False
 
 def check_neg(e):
 	pass
 
 def check_eq(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_not_eq(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_gr_than(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_le_than(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_gr_than_or_eq(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_le_than_or_eq(e):
-	pass
+	if(has_same_type(e.lhs, e.rhs)):
+		return BoolType
 
 def check_id(e):
-	pass
+	return e.ref.type
 
 def check_abs(e):
-	pass
+	t1 = e.var.type_check
+	t2 = check(e.expr)
+	return ArrowType(t1, t2)
 
 def check_app(e):
-	pass
+	t1 = check(e.lhs)
+
+	if type(t1) is not ArrowType:
+		assert False
+	if not is_same_type(t1.param, t1):
+		assert False
+
+	return t1
 
 def do_check(e):
 
